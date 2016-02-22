aa4403723f12e58ac77c074f208651648145d021
Sky Boydston <skyb@activewebsite.com>
2016-02-22 16:02:20 -0700

    added seeding for user table

--- usbsinc/app/Http/Middleware/RedirectIfAuthenticated.php
+++ usbsinc/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,8 +18,8 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            
-            return redirect('/company');
+
+            return redirect('/home');
         }
 
         return $next($request);
