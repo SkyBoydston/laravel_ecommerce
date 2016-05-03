4a60fa788cf535e89a55413de7ce60fa747e7a4d
SkyBoydston <sky.boydston@gmail.com>
2016-05-02 19:27:37 -0600

    removed public site branding

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>USBS Advantage</title>
+    <title>E-commerce System</title>
 
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
@@ -51,7 +51,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    USBS Advantage Public Site
+                    <img src="/swoosh_logo/greySwoosh.png" style="width:100px;height:auto;" />
                 </a>
             </div>
 
@@ -64,7 +64,7 @@
                     <!-- Authentication Links -->
                     @if (Auth::guest())
                         <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
+                        <!-- <li><a href="{{ url('/register') }}">Register</a></li> -->
                     
                     @endif
                 </ul>
